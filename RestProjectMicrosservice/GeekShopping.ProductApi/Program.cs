6e7e8693f9d310495253f1a0273e0baaa151cce7
Wesley <wesdias5@outlook.com>
2023-08-12 19:04:02 -0300

    Commit 3 Wesley

--- RestProjectMicrosservice/GeekShopping.ProductApi/Program.cs
+++ RestProjectMicrosservice/GeekShopping.ProductApi/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductApi.Config;
 using GeekShopping.ProductApi.Model.Context;
+using GeekShopping.ProductApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<SqlServerContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnectionString")));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- RestProjectMicrosservice/GeekShopping.ProductApi/Repository/IProdutoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using GeekShopping.ProductApi.Data.ValueObjects;
-
-namespace GeekShopping.ProductApi.Repository
-{
-    public interface IProdutoRepository
-    {
-        Task<IEnumerable<ProductVO>> FindAll();
-    }
-}
