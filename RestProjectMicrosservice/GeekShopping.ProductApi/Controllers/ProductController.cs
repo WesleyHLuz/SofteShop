34a35b395796533eb6f106526ce5f7649858cfab
Wesley <wesdias5@outlook.com>
2023-08-12 21:39:20 -0300

    Organizing The First Microsservices Architeture

---
+++ RestProjectMicrosservice/GeekShopping.ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using GeekShopping.ProductApi.Data.ValueObjects;
+using GeekShopping.ProductApi.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerator<ProductVO>>> FindAll()
+        {
+            var product = await _repository.FindAll();
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> FindById(long id)
+        {
+            var products = await _repository.FindById(id);
+            if (products == null) return NotFound();
+
+            return Ok(products);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Create(vo);
+
+
+            return Ok(product);
+        } 
+        
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Create(vo);
+
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest(); 
+            return Ok(status);
+
+
+        }
+       
+    }
+}
